6e95a6fda818958827669a92d7df3bb6b43884f8
De <spark49942@gmail.com>
2023-07-17 02:10:34 +0300

    DZ-3-rndsample

--- mod_rookgame.py
+++ mod_rookgame.py
@@ -1,5 +1,6 @@
 from numpy import array
 from random import randint
+from random import sample
 
 _board = array([[0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -11,7 +12,7 @@ _board = array([[0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0]])
 field = _board
 
-def bigame(flawless = True, _rook = None):
+def biggame(flawless = True, _rook = None):
     for i in range(len(_board)):
         for j in range(len(_board)):
             if _board[i][j] == 11:
@@ -29,18 +30,18 @@ def bigame(flawless = True, _rook = None):
 def setting(cols, lines):
     for i in range(len(_board)):
         _board[cols[i]][lines[i]] = 11
-    print(f"{_board}\n\nВсе 8 ферзей живут дружно?\n {bigame()}")
+    print(f"{_board}\n\nВсе 8 ферзей живут дружно?\n {biggame()}")
     for i in range(len(_board)):
         _board[cols[i]][lines[i]] = 0
 
 def rndgame():
     for s in range(4):
+        c = sample(range(8), 8)
         cols = list()
         lines = list()
         i = 0
         while i < 8:
-            c = randint(0, 7)
-            cols.append(c)
+            cols.append(c[i])
             l = randint(0, 7)
             lines.append(l)
             i += 1
@@ -50,3 +51,5 @@ def rndgame():
 col = [0, 1, 2, 3, 4, 5, 6, 7]
 line = [3, 0, 4, 7, 1, 6, 2, 5]
 setting(col, line)
+
+rndgame()
